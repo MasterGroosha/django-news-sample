628b0054a1585e6932cee2abeef379f42e1fcc75
Groosha <groosha@protonmail.com>
2023-12-02 14:12:09 +0800

    Added simple index view for news app

--- newswebsite/news/views.py
+++ newswebsite/news/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from datetime import date, timedelta
+
+from django.http import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
+
+
+def available_dates():
+    dates = list()
+    starting_date = date(year=2023, month=11, day=1)
+    while starting_date.month != 12:
+        dates.append(starting_date)
+        starting_date += timedelta(days=1)
+    return dates
+
+
+def index(request):
+    template = loader.get_template("news/index.html")
+    context = {
+        "available_dates": available_dates()
+    }
+    return HttpResponse(template.render(context, request))
